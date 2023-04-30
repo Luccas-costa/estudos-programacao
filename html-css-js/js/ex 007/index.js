4d45662a2471ca385489513a3f1fe1448e5e90ac
luccas costa <lucaspcosta70@gmail.com>
2023-04-30 15:53:50 -0300

    Estudos Programacao

---
+++ html-css-js/js/ex 007/index.js
@@ -0,0 +1,9 @@
+var vel = 78
+
+console.log(`A velocidade do seu carro é ${vel}km/h`)
+
+if (vel > 60) {
+    console.log(`Voce ultrapassou a velocidade permetida. Multado!`)
+}
+
+console.log('dirija sempre usando cinto de segurança!')
